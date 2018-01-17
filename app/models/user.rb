3e5bda31caa7190b4420047219cb6d1f070014d4
Alex Nsia <alexnsia@gmail.com>
2018-01-17 19:46:44 +0100

    models

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
   def generate_token(column)
     begin
       self[column] = SecureRandom.urlsafe_base64
-    end while User.exists?(column => self[column])
+    end while User.exists(column => self[column])
   end
 
 
