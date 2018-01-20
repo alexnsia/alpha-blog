59e69c790a731562b3ab0d57726787be68d89e86
Alex Nsia <alexnsia@gmail.com>
2018-01-20 21:54:19 +0100

    Modify db schema

--- db/schema.rb
+++ db/schema.rb
@@ -39,9 +39,6 @@ ActiveRecord::Schema.define(version: 20180120091931) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
-    t.string "auth_token"
-    t.string "password_reset_token"
-    t.datetime "password_reset_sent_at"
     t.boolean "admin", default: false
   end
 
