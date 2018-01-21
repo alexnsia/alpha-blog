d26128d893240885a0b145396c547a90860c2c08
Alex Nsia <alexnsia@gmail.com>
2018-01-21 20:00:05 +0100

    Fixing error

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -27,7 +27,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     end
     assert_template 'categories/new'
     assert_select 'h2.panel-title'
-    assert_select 'h2.panel-body'
+    assert_select 'div.panel-body'
   end
 
 end
